6585fabba17113f5851ef00569f8684cdf56abd4
Jim McGlone <jmcglone5572@sbcglobal.net>
2019-09-03 08:06:24 -0700

    Misc.

--- CrudFramework.Contracts/EntityDeletedEvent.cs
+++ CrudFramework.Contracts/EntityDeletedEvent.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CrudFramework.Contracts
+﻿namespace CrudFramework.Contracts
 {
     public class EntityDeletedEvent<TEntity> : IEvent
     {
